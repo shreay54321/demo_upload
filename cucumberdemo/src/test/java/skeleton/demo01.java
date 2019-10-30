0c04a092ee01c1f3bf13e00addd4829e1ac1640d
soumya.c.mishra <Training_B4A.01.18@BDC4-D-J0FZQG2.dir.svc.accenture.com>
2019-10-30 16:51:23 +0530

    ggkigkhk,kh

---
+++ cucumberdemo/src/test/java/skeleton/demo01.java
@@ -0,0 +1,13 @@
+package skeleton;
+
+import cucumber.api.java.en.Given;
+
+public class demo01 {
+	@Given("(john|Rita) has {int} cucumber(s) in (his/her) bag")
+	public void john_has_cucumber_in_his_bag(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	
+}
